f8720d720ada5a6b71b1bc53a173b309611735c2
Nazmus Sakib <nazmus767921@gmail.com>
2023-08-30 18:51:12 +0600

    #

--- src/styles/layout.js
+++ src/styles/layout.js
@@ -1,7 +1,14 @@
 import { styled } from "styled-components";
+import { devices } from "./devices";
 
 export const PageContainer = styled.section`
 	width: 90%;
 	margin: 0 auto;
+	@media only screen and (${devices.md}) {
+		width: 80%;
+	}
+	@media only screen and (${devices["2xl"]}) {
+		width: 70%;
+	}
 	/* overflow: hidden; */
 `;
